8db34607378193c183e21fc0a78b990bccb64441
wannabel94-dot <w.annabel94@gmail.com>
2025-11-08 22:53:02 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,8 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('app-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css'
-      ]);
-    })
-  );
+// /service-worker.js
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  // Optional: implement caching here
 });
