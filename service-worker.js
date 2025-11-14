04cdc106070470f6e63742368d1251b6bc25c37d
wannabel94-dot <w.annabel94@gmail.com>
2025-11-14 23:38:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,3 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   console.log('Service worker activated');
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
